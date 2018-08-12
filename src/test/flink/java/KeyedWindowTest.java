3f36e607e8ed2168e25f8e40bb004b4c3f211981
yuan <zhao137578346@gmail.com>
2018-08-12 14:20:20 +0800

    Enhancement:add test based on java

---
+++ src/test/flink/java/KeyedWindowTest.java
@@ -0,0 +1,20 @@
+package flink.java;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class KeyedWindowTest {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.fromElements(1, 1, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7)
+                .keyBy(e -> e)
+                .countWindow(2)
+                .sum(0)
+                .setParallelism(1)
+                .print()
+                .setParallelism(1);
+
+        env.execute();
+    }
+}
