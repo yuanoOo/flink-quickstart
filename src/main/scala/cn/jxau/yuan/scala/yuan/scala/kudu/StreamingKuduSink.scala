15d7311cc3bbb1a136428ff01418734f9e81e525
zhaomingyuan <zhaomingyuan@etouch.cn>
2018-08-17 12:48:23 +0800

    Enhancement:update flink app main class in POM

---
+++ src/main/scala/cn/jxau/yuan/scala/yuan/scala/kudu/StreamingKuduSink.scala
@@ -0,0 +1,21 @@
+package cn.jxau.yuan.scala.yuan.scala.kudu
+
+import org.apache.flink.streaming.api.scala._
+
+/**
+  * @author zhaomingyuan
+  * @date 18-8-16
+  * @time 下午5:45
+  */
+object StreamingKuduSink {
+//
+//    def main(args: Array[String]): Unit = {
+//        /* Streaming mode - DataSream API - *//* Streaming mode - DataSream API - */
+//        val env = StreamExecutionEnvironment.getExecutionEnvironment
+//
+//        val stream = env.fromElements("data1 data2 data3")
+//        val stream2 = stream.map(new MapToRowSerializable())
+//
+//        stream2.addSink(new Nothing(KUDU_MASTER, DEST_TABLE, columnNames))
+//    }
+}
