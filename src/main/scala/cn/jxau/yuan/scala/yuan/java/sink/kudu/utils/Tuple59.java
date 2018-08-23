b931ba6002f4d88f877afbc817df7acd28806534
zhaomingyuan <zhaomingyuan@etouch.cn>
2018-08-23 18:42:35 +0800

    Enhancement:add kuduTuple59

---
+++ src/main/scala/cn/jxau/yuan/scala/yuan/java/sink/kudu/utils/Tuple59.java
@@ -0,0 +1,347 @@
+package cn.jxau.yuan.scala.yuan.java.sink.kudu.utils;
+
+
+import kudu.internal.KuduTuple;
+
+public class Tuple59<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42, T43, T44, T45, T46, T47, T48, T49, T50, T51, T52, T53, T54, T55, T56, T57, T58, T59> extends KuduTuple{
+
+    private static final long serialVersionUID = 1L;
+
+    /** Field 0 of the tuple. */
+    public T0 f0;
+    /** Field 1 of the tuple. */
+    public T1 f1;
+    /** Field 2 of the tuple. */
+    public T2 f2;
+    /** Field 3 of the tuple. */
+    public T3 f3;
+    /** Field 4 of the tuple. */
+    public T4 f4;
+    /** Field 5 of the tuple. */
+    public T5 f5;
+    /** Field 6 of the tuple. */
+    public T6 f6;
+    /** Field 7 of the tuple. */
+    public T7 f7;
+    /** Field 8 of the tuple. */
+    public T8 f8;
+    /** Field 9 of the tuple. */
+    public T9 f9;
+    /** Field 10 of the tuple. */
+    public T10 f10;
+    /** Field 11 of the tuple. */
+    public T11 f11;
+    /** Field 12 of the tuple. */
+    public T12 f12;
+    /** Field 13 of the tuple. */
+    public T13 f13;
+    /** Field 14 of the tuple. */
+    public T14 f14;
+    /** Field 15 of the tuple. */
+    public T15 f15;
+    /** Field 16 of the tuple. */
+    public T16 f16;
+    /** Field 17 of the tuple. */
+    public T17 f17;
+    /** Field 18 of the tuple. */
+    public T18 f18;
+    /** Field 19 of the tuple. */
+    public T19 f19;
+    /** Field 20 of the tuple. */
+    public T20 f20;
+    /** Field 21 of the tuple. */
+    public T21 f21;
+    /** Field 22 of the tuple. */
+    public T22 f22;
+    /** Field 23 of the tuple. */
+    public T23 f23;
+    /** Field 24 of the tuple. */
+    public T24 f24;
+    public T25 f25;
+    public T26 f26;
+    public T27 f27;
+    public T28 f28;
+    public T29 f29;
+    public T30 f30;
+    public T31 f31;
+    public T32 f32;
+    public T33 f33;
+    public T34 f34;
+    public T35 f35;
+    public T36 f36;
+    public T37 f37;
+    public T38 f38;
+    public T39 f39;
+    public T40 f40;
+    public T41 f41;
+    public T42 f42;
+    public T43 f43;
+    public T44 f44;
+    public T45 f45;
+    public T46 f46;
+    public T47 f47;
+    public T48 f48;
+    public T49 f49;
+    public T50 f50;
+    public T51 f51;
+    public T52 f52;
+    public T53 f53;
+    public T54 f54;
+    public T55 f55;
+    public T56 f56;
+    public T57 f57;
+    public T58 f58;
+    public T59 f59;
+
+    public Tuple59() {
+    }
+
+    public Tuple59(T0 f0, T1 f1, T2 f2, T3 f3, T4 f4, T5 f5, T6 f6, T7 f7, T8 f8, T9 f9, T10 f10, T11 f11, T12 f12, T13 f13, T14 f14, T15 f15, T16 f16, T17 f17, T18 f18, T19 f19, T20 f20, T21 f21, T22 f22, T23 f23, T24 f24, T25 f25, T26 f26, T27 f27, T28 f28, T29 f29, T30 f30, T31 f31, T32 f32, T33 f33, T34 f34, T35 f35, T36 f36, T37 f37, T38 f38, T39 f39, T40 f40, T41 f41, T42 f42, T43 f43, T44 f44, T45 f45, T46 f46, T47 f47, T48 f48, T49 f49, T50 f50, T51 f51, T52 f52, T53 f53, T54 f54, T55 f55, T56 f56, T57 f57, T58 f58, T59 f59) {
+        this.f0 = f0;
+        this.f1 = f1;
+        this.f2 = f2;
+        this.f3 = f3;
+        this.f4 = f4;
+        this.f5 = f5;
+        this.f6 = f6;
+        this.f7 = f7;
+        this.f8 = f8;
+        this.f9 = f9;
+        this.f10 = f10;
+        this.f11 = f11;
+        this.f12 = f12;
+        this.f13 = f13;
+        this.f14 = f14;
+        this.f15 = f15;
+        this.f16 = f16;
+        this.f17 = f17;
+        this.f18 = f18;
+        this.f19 = f19;
+        this.f20 = f20;
+        this.f21 = f21;
+        this.f22 = f22;
+        this.f23 = f23;
+        this.f24 = f24;
+        this.f25 = f25;
+        this.f26 = f26;
+        this.f27 = f27;
+        this.f28 = f28;
+        this.f29 = f29;
+        this.f30 = f30;
+        this.f31 = f31;
+        this.f32 = f32;
+        this.f33 = f33;
+        this.f34 = f34;
+        this.f35 = f35;
+        this.f36 = f36;
+        this.f37 = f37;
+        this.f38 = f38;
+        this.f39 = f39;
+        this.f40 = f40;
+        this.f41 = f41;
+        this.f42 = f42;
+        this.f43 = f43;
+        this.f44 = f44;
+        this.f45 = f45;
+        this.f46 = f46;
+        this.f47 = f47;
+        this.f48 = f48;
+        this.f49 = f49;
+        this.f50 = f50;
+        this.f51 = f51;
+        this.f52 = f52;
+        this.f53 = f53;
+        this.f54 = f54;
+        this.f55 = f55;
+        this.f56 = f56;
+        this.f57 = f57;
+        this.f58 = f58;
+        this.f59 = f59;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getField(int pos) {
+        switch(pos) {
+            case 0: return (T) this.f0;
+            case 1: return (T) this.f1;
+            case 2: return (T) this.f2;
+            case 3: return (T) this.f3;
+            case 4: return (T) this.f4;
+            case 5: return (T) this.f5;
+            case 6: return (T) this.f6;
+            case 7: return (T) this.f7;
+            case 8: return (T) this.f8;
+            case 9: return (T) this.f9;
+            case 10: return (T) this.f10;
+            case 11: return (T) this.f11;
+            case 12: return (T) this.f12;
+            case 13: return (T) this.f13;
+            case 14: return (T) this.f14;
+            case 15: return (T) this.f15;
+            case 16: return (T) this.f16;
+            case 17: return (T) this.f17;
+            case 18: return (T) this.f18;
+            case 19: return (T) this.f19;
+            case 20: return (T) this.f20;
+            case 21: return (T) this.f21;
+            case 22: return (T) this.f22;
+            case 23: return (T) this.f23;
+            case 24: return (T) this.f24;
+            case 25: return (T) this.f25;
+            case 26: return (T) this.f26;
+            case 27: return (T) this.f27;
+            case 28: return (T) this.f28;
+            case 29: return (T) this.f29;
+            case 30: return (T) this.f30;
+            case 31: return (T) this.f31;
+            case 32: return (T) this.f32;
+            case 33: return (T) this.f33;
+            case 34: return (T) this.f34;
+            case 35: return (T) this.f35;
+            case 36: return (T) this.f36;
+            case 37: return (T) this.f37;
+            case 38: return (T) this.f38;
+            case 39: return (T) this.f39;
+            case 40: return (T) this.f40;
+            case 41: return (T) this.f41;
+            case 42: return (T) this.f42;
+            case 43: return (T) this.f43;
+            case 44: return (T) this.f44;
+            case 45: return (T) this.f45;
+            case 46: return (T) this.f46;
+            case 47: return (T) this.f47;
+            case 48: return (T) this.f48;
+            case 49: return (T) this.f49;
+            case 50: return (T) this.f50;
+            case 51: return (T) this.f51;
+            case 52: return (T) this.f52;
+            case 53: return (T) this.f53;
+            case 54: return (T) this.f54;
+            case 55: return (T) this.f55;
+            case 56: return (T) this.f56;
+            case 57: return (T) this.f57;
+            case 58: return (T) this.f58;
+            case 59: return (T) this.f59;
+            default: throw new IndexOutOfBoundsException(String.valueOf(pos));
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> void setField(T value, int pos) {
+        switch(pos) {
+            case 0:
+                this.f0 = (T0) value;
+                break;
+            case 1:
+                this.f1 = (T1) value;
+                break;
+            case 2:
+                this.f2 = (T2) value;
+                break;
+            case 3:
+                this.f3 = (T3) value;
+                break;
+            case 4:
+                this.f4 = (T4) value;
+                break;
+            case 5:
+                this.f5 = (T5) value;
+                break;
+            case 6:
+                this.f6 = (T6) value;
+                break;
+            case 7:
+                this.f7 = (T7) value;
+                break;
+            case 8:
+                this.f8 = (T8) value;
+                break;
+            case 9:
+                this.f9 = (T9) value;
+                break;
+            case 10:
+                this.f10 = (T10) value;
+                break;
+            case 11:
+                this.f11 = (T11) value;
+                break;
+            case 12:
+                this.f12 = (T12) value;
+                break;
+            case 13:
+                this.f13 = (T13) value;
+                break;
+            case 14:
+                this.f14 = (T14) value;
+                break;
+            case 15:
+                this.f15 = (T15) value;
+                break;
+            case 16:
+                this.f16 = (T16) value;
+                break;
+            case 17:
+                this.f17 = (T17) value;
+                break;
+            case 18:
+                this.f18 = (T18) value;
+                break;
+            case 19:
+                this.f19 = (T19) value;
+                break;
+            case 20:
+                this.f20 = (T20) value;
+                break;
+            case 21:
+                this.f21 = (T21) value;
+                break;
+            case 22:
+                this.f22 = (T22) value;
+                break;
+            case 23:
+                this.f23 = (T23) value;
+                break;
+            case 24:
+                this.f24 = (T24) value;
+                break;
+            case 25:this.f25 = (T25) value;break;
+            case 26:this.f26 = (T26) value;break;
+            case 27:this.f27 = (T27) value;break;
+            case 28:this.f28 = (T28) value;break;
+            case 29:this.f29 = (T29) value;break;
+            case 30:this.f30 = (T30) value;break;
+            case 31:this.f31 = (T31) value;break;
+            case 32:this.f32 = (T32) value;break;
+            case 33:this.f33 = (T33) value;break;
+            case 34:this.f34 = (T34) value;break;
+            case 35:this.f35 = (T35) value;break;
+            case 36:this.f36 = (T36) value;break;
+            case 37:this.f37 = (T37) value;break;
+            case 38:this.f38 = (T38) value;break;
+            case 39:this.f39 = (T39) value;break;
+            case 40:this.f40 = (T40) value;break;
+            case 41:this.f41 = (T41) value;break;
+            case 42:this.f42 = (T42) value;break;
+            case 43:this.f43 = (T43) value;break;
+            case 44:this.f44 = (T44) value;break;
+            case 45:this.f45 = (T45) value;break;
+            case 46:this.f46 = (T46) value;break;
+            case 47:this.f47 = (T47) value;break;
+            case 48:this.f48 = (T48) value;break;
+            case 49:this.f49 = (T49) value;break;
+            case 50:this.f50 = (T50) value;break;
+            case 51:this.f51 = (T51) value;break;
+            case 52:this.f52 = (T52) value;break;
+            case 53:this.f53 = (T53) value;break;
+            case 54:this.f54 = (T54) value;break;
+            case 55:this.f55 = (T55) value;break;
+            case 56:this.f56 = (T56) value;break;
+            case 57:this.f57 = (T57) value;break;
+            case 58:this.f58 = (T58) value;break;
+            case 59:this.f59 = (T59) value;break;
+            default: throw new IndexOutOfBoundsException(String.valueOf(pos));
+        }
+    }
+}
