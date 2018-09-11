ecf7fb607ad4f7fd4f4c4d989bde12bab0175d1e
zhaomingyuan <zhaomingyuan@etouch.cn>
2018-09-11 11:50:29 +0800

    Enhancement:refine bloom-filter based ValueState

--- src/main/scala/cn/jxau/yuan/scala/yuan/scala/state/function/BloomFilterInValueState.scala
+++ src/main/scala/cn/jxau/yuan/scala/yuan/scala/state/function/BloomFilterInValueState.scala
@@ -5,7 +5,6 @@ import com.google.common.hash.{BloomFilter, Funnel, Sink}
 import org.apache.flink.api.common.ExecutionConfig
 import org.apache.flink.api.common.functions.RichFilterFunction
 import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
-import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.java.typeutils.runtime.kryo.KryoSerializer
 import org.apache.flink.configuration.Configuration
 import org.slf4j.LoggerFactory
@@ -15,20 +14,18 @@ import org.slf4j.LoggerFactory
   * @date 18-9-10
   * @time 下午4:25
   */
-class BloomFilterInValueState extends RichFilterFunction[Tuple2[Long, Long]]{
+class BloomFilterInValueState extends RichFilterFunction[(Long, Long)]{
 
     private val LOG = LoggerFactory.getLogger(classOf[BloomFilterInValueState])
     private val BLOOM_FPP = 0.0000001d
     private val EXPECTED_INSERTIONS = 1024 * 1024 * 32
-
     private var sum: ValueState[BloomFilter[String]] = _
 
-    override def filter(value: Tuple2[Long, Long]): Boolean = {
-        val bloomFilter: BloomFilter[String] = BloomFilter.create(new Funnel[String] {
-            override def funnel(from: String, into: Sink): Unit = into.putString(from, Charsets.UTF_8)
-        }, EXPECTED_INSERTIONS, BLOOM_FPP)
-        if (sum.value() == null)
+    override def filter(value: (Long, Long)): Boolean = {
+        if (sum.value() == null){
+            val bloomFilter: BloomFilter[String] = BloomFilter.create(new Funnel[String] {override def funnel(from: String, into: Sink): Unit = into.putString(from, Charsets.UTF_8)}, EXPECTED_INSERTIONS, BLOOM_FPP)
             sum.update(bloomFilter)
+        }
 
         !sum.value().mightContain(value._1.toString)
     }
@@ -36,8 +33,6 @@ class BloomFilterInValueState extends RichFilterFunction[Tuple2[Long, Long]]{
     override def open(parameters: Configuration): Unit = {
         val config = new ExecutionConfig
         val serializer = new KryoSerializer[BloomFilter[String]](classOf[BloomFilter[String]], config)
-
-
         val descriptor = new ValueStateDescriptor[BloomFilter[String]]("bloom-filter", serializer)
 
         sum = getRuntimeContext.getState(descriptor)
