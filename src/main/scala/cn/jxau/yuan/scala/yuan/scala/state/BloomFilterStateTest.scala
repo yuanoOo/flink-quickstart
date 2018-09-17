ae7ea848310b4231946f687b191e794863c8652f
zhaomingyuan <zhaomingyuan@etouch.cn>
2018-09-17 21:24:47 +0800

    Enhancement:refine local bloom filter to stat uv...

---
+++ src/main/scala/cn/jxau/yuan/scala/yuan/scala/state/BloomFilterStateTest.scala
@@ -0,0 +1,40 @@
+package cn.jxau.yuan.scala.yuan.scala.state
+
+import cn.jxau.yuan.scala.yuan.scala.state.function.BloomFilterInValueState
+import org.apache.flink.configuration.{Configuration, CoreOptions}
+import org.apache.flink.runtime.state.filesystem.FsStateBackend
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
+import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
+
+import scala.util.Random
+
+/**
+  * @author zhaomingyuan
+  * @date 18-9-17
+  * @time 下午6:46
+  */
+object BloomFilterStateTest {
+
+    def main(args: Array[String]): Unit = {
+
+        //--------------------本地容错配置----------------------//
+        val conf = new Configuration
+        conf.setString(CoreOptions.CHECKPOINTS_DIRECTORY, "file:///home/yuan/test/checkpoint")
+        val env = StreamExecutionEnvironment.createLocalEnvironment(1, conf)
+        env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+        env.enableCheckpointing(5000, CheckpointingMode.EXACTLY_ONCE)
+        env.setStateBackend(new FsStateBackend("file:///home/yuan/test/checkpoint"))
+
+        env.addSource((context: SourceContext[String]) => {while(true) context.collect(new Random().nextInt(1000) + ":FRI")})
+                .setParallelism(1)
+                .keyBy(s => s.endsWith(":FRI"))
+                .filter(new BloomFilterInValueState)
+                .setParallelism(1)
+                .addSink(new SinkFunction[String] {})
+                .setParallelism(1)
+
+        env.execute()
+    }
+}
--- src/main/scala/cn/jxau/yuan/scala/yuan/scala/state/function/BloomFilterInValueState.scala
+++ src/main/scala/cn/jxau/yuan/scala/yuan/scala/state/function/BloomFilterInValueState.scala
@@ -1,10 +1,10 @@
 package cn.jxau.yuan.scala.yuan.scala.state.function
 
-import com.google.common.base.Charsets
-import com.google.common.hash.{BloomFilter, Funnel, Sink}
+import com.google.common.hash._
 import org.apache.flink.api.common.ExecutionConfig
 import org.apache.flink.api.common.functions.RichFilterFunction
 import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
+import org.apache.flink.api.common.typeinfo.{TypeHint, TypeInformation}
 import org.apache.flink.api.java.typeutils.runtime.kryo.KryoSerializer
 import org.apache.flink.configuration.Configuration
 import org.slf4j.LoggerFactory
@@ -14,27 +14,55 @@ import org.slf4j.LoggerFactory
   * @date 18-9-10
   * @time 下午4:25
   */
-class BloomFilterInValueState extends RichFilterFunction[(Long, Long)]{
+class BloomFilterInValueState extends RichFilterFunction[String] {
 
     private val LOG = LoggerFactory.getLogger(classOf[BloomFilterInValueState])
-    private val BLOOM_FPP = 0.0000001d
-    private val EXPECTED_INSERTIONS = 1024 * 1024 * 32
-    private var sum: ValueState[BloomFilter[String]] = _
+    private val BLOOM_FPP: Double = 0.0001d
+    private val EXPECTED_INSERTIONS = 1024 * 32
+    private def bloomFilter: BloomFilter[CharSequence] = BloomFilter.create(Funnels.stringFunnel(), EXPECTED_INSERTIONS, BLOOM_FPP)
 
-    override def filter(value: (Long, Long)): Boolean = {
-        if (sum.value() == null){
-            val bloomFilter: BloomFilter[String] = BloomFilter.create(new Funnel[String] {override def funnel(from: String, into: Sink): Unit = into.putString(from, Charsets.UTF_8)}, EXPECTED_INSERTIONS, BLOOM_FPP)
+    private var localBloom: BloomFilter[CharSequence] = _
+    private var sum: ValueState[BloomFilter[CharSequence]] = _
+    private var point: ValueState[Long] = _
+    private var longPoint: Long = _
+
+    override def filter(value: String): Boolean = {
+        if (sum.value() == null) {
             sum.update(bloomFilter)
+            localBloom = sum.value()
+            longPoint = point.value()
         }
 
-        !sum.value().mightContain(value._1.toString)
+        if (!localBloom.mightContain(value)) {
+            localBloom.put(value)
+            longPoint += 1
+            println(value + "===> false")
+        }
+
+        if (longPoint >= EXPECTED_INSERTIONS) rotaBloomFilter()
+
+        !sum.value().mightContain(value)
     }
 
     override def open(parameters: Configuration): Unit = {
         val config = new ExecutionConfig
-        val serializer = new KryoSerializer[BloomFilter[String]](classOf[BloomFilter[String]], config)
-        val descriptor = new ValueStateDescriptor[BloomFilter[String]]("bloom-filter", serializer)
+        val serializer = new KryoSerializer[BloomFilter[CharSequence]](classOf[BloomFilter[CharSequence]], config)
+        val descriptor = new ValueStateDescriptor[BloomFilter[CharSequence]]("bloom-filter", serializer)
 
         sum = getRuntimeContext.getState(descriptor)
+
+        val pointDescriptor = new ValueStateDescriptor[Long]("point", new TypeHint[Long]() {}.getTypeInfo)
+        point = getRuntimeContext.getState(pointDescriptor)
+    }
+
+    def rotaBloomFilter(): Unit = {
+        sum.update(bloomFilter)
+        localBloom = sum.value()
+        println(localBloom.toString)
+        LOG.info("init bloom filter...")
+
+        point.update(0)
+        longPoint = point.value()
+        LOG.info("init point...")
     }
 }
